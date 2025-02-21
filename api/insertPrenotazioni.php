7978c1d44e2574aecfe8ad2bf301cbe15b1a71ad
anastasia28git <anastasia.faggianelli@gmail.com>
2025-02-21 12:43:00 +0100

    insertPrenotazioni

---
+++ api/insertPrenotazioni.php
@@ -0,0 +1,47 @@
+<?php
+header("Access-Control-Allow-Origin: *"); // Permette richieste da qualsiasi dominio (*), cambialo per sicurezza
+header("Access-Control-Allow-Methods:  POST, OPTIONS"); // Metodi consentiti
+header("Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With"); // Header consentiti
+header("Access-Control-Allow-Credentials: true"); 
+header("Content-Type: application/json");
+// Pulisce il buffer senza inviarlo
+ob_start();
+ob_end_clean();
+error_reporting(0);
+ini_set('display_errors', 0);
+if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+    exit; // Rispondi subito per le richieste preflight
+}
+
+
+
+        $conn=new mysqli("localhost","root","","z-planning_db");
+              
+
+        if($conn->error) {
+            echo json_encode(['errore' => 'nessunrisultato']);
+            die();
+        }
+        else {
+
+
+            $rawData = file_get_contents("php://input");
+            $dati = json_decode($rawData, true);
+            $id_utente = $dati['id_utente'];
+            $id_postazione = $dati['id_postazione'];
+            $data = $dati['data'];
+            $sql= "INSERT INTO prenotazioni VALUES (null, '$id_utente', '$id_postazione', '$data')";
+            $result=$conn->query($sql);
+
+                $records = [];
+
+                if ($result->num_rows > 0) {
+                    while ($row = $result->fetch_assoc()) {
+                        $records[] = $row;
+                    }
+                }
+
+                echo json_encode($records);
+                $conn->close();
+            }
+?>
