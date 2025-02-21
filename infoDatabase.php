4917862481a4b0be9707bbff21ebf2dbd0a6532f
Airali06 <airali.cld@gmail.com>
2025-02-21 11:37:23 +0100

    istruzioni

---
+++ infoDatabase.php
@@ -0,0 +1,44 @@
+/*
+
+UTENTE
+
+id_utente
+nome
+cognome
+username
+password
+livello
+id_coordinatore
+
+/////////////////////////////////////////////
+
+PRENOTAZIONI
+
+id_prenotazione
+id_utente
+id_postazione
+data
+n_modifiche
+flag
+
+//////////////////////////////////////////////
+
+POSTAZIONI
+
+id_postazione
+nome
+id_categoria
+drscrizione
+stato
+
+//////////////////////////////////////////////
+
+
+QUERY
+
+dato id coordinatore prendere i suoi utenti
+dato id_utente e nuova password impostare la nuova password 
+date le informazioni della prenotazione aggiungerla (n modifiche e flag sempre partenti da 0)
+date info utente aggiungerlo
+
+*/
