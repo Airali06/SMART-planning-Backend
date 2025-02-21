70195eb2a17c1fde955d06ddeab3f15c0237490e
anastasia28git <anastasia.faggianelli@gmail.com>
2025-02-21 13:30:19 +0100

    checkPostazioniOccupate

---
+++ api/checkPostazioniOccupate.php
@@ -0,0 +1,44 @@
+<?php
+header("Access-Control-Allow-Origin: *"); // Permette richieste da qualsiasi dominio (*), cambialo per sicurezza
+header("Access-Control-Allow-Methods:  POST, OPTIONS"); // Metodi consentiti
+header("Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With"); // Header consentiti
+header("Access-Control-Allow-Credentials: true"); 
+header("Content-Type: application/json");
+// Pulisce il buffer senza inviarlo
+ob_start();
+ob_end_clean();
+error_reporting(0);
+ini_set('display_errors', 0);
+if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+    exit; // Rispondi subito per le richieste preflight
+}
+
+
+
+        $conn=new mysqli("localhost","root","","z-planning_db");
+        $rawData = file_get_contents("php://input");
+        $dati = json_decode($rawData, true);
+        $id_postazione = $dati['id_postazione'];
+        $data = $dati['data']; 
+
+        if($conn->error) {
+            echo json_encode(['errore' => 'nessunrisultato']);
+            die();
+        }
+        else {
+
+            $sql="SELECT id_prenotazione FROM prenotazione WHERE data = '$data', id_postazione = '$id_postazione'";
+            $result=$conn->query($sql);
+
+                $records = [];
+
+                if ($result->num_rows > 0) {
+                    echo json_encode(['stato' => 'occupata']);
+                    die();
+                }else{
+                    echo json_encode(['stato' => 'libera']);
+                    die();
+                }
+                $conn->close();
+
+}
